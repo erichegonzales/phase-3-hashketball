84ce29f0e3bf2ea5decd0a267ccb4476f016a387
Eriche Gonzales <erichelouise@gmail.com>
2022-08-10 21:47:19 -0400

    commit tree 37946df9563670033cd336b49fb6999c11571493

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 # Write your code below game_hash
 def game_hash
   {
@@ -14,7 +16,7 @@ def game_hash
           assists: 12,
           steals: 3,
           blocks: 1,
-          slam_dunks: 1
+          slam_dunks: 1,
         },
         {
           player_name: "Reggie Evans",
@@ -25,7 +27,7 @@ def game_hash
           assists: 12,
           steals: 12,
           blocks: 12,
-          slam_dunks: 7
+          slam_dunks: 7,
         },
         {
           player_name: "Brook Lopez",
@@ -36,7 +38,7 @@ def game_hash
           assists: 10,
           steals: 3,
           blocks: 1,
-          slam_dunks: 15
+          slam_dunks: 15,
         },
         {
           player_name: "Mason Plumlee",
@@ -47,7 +49,7 @@ def game_hash
           assists: 6,
           steals: 3,
           blocks: 8,
-          slam_dunks: 5
+          slam_dunks: 5,
         },
         {
           player_name: "Jason Terry",
@@ -58,9 +60,9 @@ def game_hash
           assists: 2,
           steals: 4,
           blocks: 11,
-          slam_dunks: 1
-        }
-      ]
+          slam_dunks: 1,
+        },
+      ],
     },
     away: {
       team_name: "Charlotte Hornets",
@@ -75,7 +77,7 @@ def game_hash
           assists: 1,
           steals: 2,
           blocks: 7,
-          slam_dunks: 2
+          slam_dunks: 2,
         },
         {
           player_name: "Bismack Biyombo",
@@ -86,7 +88,7 @@ def game_hash
           assists: 7,
           steals: 22,
           blocks: 15,
-          slam_dunks: 10
+          slam_dunks: 10,
         },
         {
           player_name: "DeSagna Diop",
@@ -97,7 +99,7 @@ def game_hash
           assists: 12,
           steals: 4,
           blocks: 5,
-          slam_dunks: 5
+          slam_dunks: 5,
         },
         {
           player_name: "Ben Gordon",
@@ -108,7 +110,7 @@ def game_hash
           assists: 2,
           steals: 1,
           blocks: 1,
-          slam_dunks: 0
+          slam_dunks: 0,
         },
         {
           player_name: "Kemba Walker",
@@ -119,11 +121,155 @@ def game_hash
           assists: 12,
           steals: 7,
           blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
+          slam_dunks: 12,
+        },
+      ],
+    },
   }
 end
 
-# Write code here
+### first attempt solution
+
+# def num_points_scored(player)
+#   game_hash.each do |team_type, team_hash|
+#     team_hash.each do |team_attribute, attribute_data|
+#       if team_attribute == :players
+#         attribute_data.each do |player_data|
+#           return player_data[:points] if player_data[:player_name] == player
+#         end
+#       end
+#     end
+#   end
+# end
+
+# def shoe_size(player)
+#   game_hash.each do |team_type, team_hash|
+#     team_hash.each do |team_attribute, attribute_data|
+#       if team_attribute == :players
+#         attribute_data.each do |player_data|
+#           return player_data[:shoe] if player_data[:player_name] == player
+#         end
+#       end
+#     end
+#   end
+# end
+
+# def team_colors(team)
+#   game_hash.each do |team_type, team_hash|
+#     return team_hash[:colors] if team_hash[:team_name] == team
+#   end
+# end
+
+# def team_names
+#   name_arr = []
+#   game_hash.each do |team_type, team_hash|
+#     name_arr.push(team_hash[:team_name])
+#   end
+#   name_arr
+# end
+
+# def player_numbers(team)
+#   jersey_arr = []
+#   game_hash.each do |team_type, team_hash|
+#     team_hash.each do |team_attribute, attribute_data|
+#       if team_attribute == :players
+#         attribute_data.each do |player_data|
+#           jersey_arr.push(player_data[:number]) if team_hash[:team_name] == team
+#         end
+#       end
+#     end
+#   end
+#   jersey_arr
+# end
+
+# def player_stats(player)
+#   game_hash.each do |team_type, team_hash|
+#     team_hash.each do |team_attribute, attribute_data|
+#       if team_attribute == :players
+#         attribute_data.each do |player_data|
+#           return player_data if player_data[:player_name] == player
+#         end
+#       end
+#     end
+#   end
+# end
+
+# def big_shoe_rebounds
+#   shoe_sizes = []
+#   game_hash.each do |team_type, team_hash|
+#     team_hash.each do |team_attribute, attribute_data|
+#       if team_attribute == :players
+#         attribute_data.each do |player_data|
+#           shoe_sizes.push(player_data[:shoe])
+#         end
+#         attribute_data.each do |player_data|
+#         return player_data[:rebounds] if player_data[:shoe] == shoe_sizes.max
+#         end
+#       end
+#     end
+#   end
+# end
+
+### github solution
+
+# helper method for accessing an array of all the players from both teams
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def num_points_scored(player_name)
+  player = player_stats(player_name)
+  player[:points]
+end
+
+def shoe_size(player_name)
+  player = player_stats(player_name)
+  player[:shoe]
+end
+
+# helper method to find a team by their team name
+def find_team(team_name)
+  # #find on with a hash returns an array with the first key and value that match the condition
+  team_info = game_hash.find do |location, team_data|
+    team_data[:team_name] == team_name
+  end
+
+  # return just the value (team_data) from the .find method
+  team_info[1]
+end
+
+def team_colors(team_name)
+  team = find_team(team_name)
+  team[:colors]
+end
+
+def team_names
+  game_hash.map do |location, team_data|
+    team_data[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  team = find_team(team_name)
+  team[:players].map do |player|
+    player[:number]
+  end
+end
+
+def player_stats(player_name)
+  all_players.find do |player|
+    player[:player_name] == player_name
+  end
+end
+
+# helper method to find the player with the biggest shoe
+def big_shoe_player
+  # https://ruby-doc.org/core-3.0.1/Enumerable.html#method-i-max_by
+  all_players.max_by do |player|
+    player[:shoe]
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe_player[:rebounds]
+end
